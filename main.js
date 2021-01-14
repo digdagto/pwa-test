21661e9239a2b58cb2ecca9ad4d3e74c28da578e
taeseokchoi <digdagto@gmail.com>
2021-01-14 23:10:16 +0900

    register the service worker

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
 if('serviceWorker' in navigator){
-    console.log("service worker work");
+    window.addEventListener('load', function(){
+        navigator.serviceWorker
+        .register('sw_cached_pages.js')
+        .then(reg => console.log('service worker registered' + reg))
+        .catch(err => console.log(`Service Worker: Error: ${err}`))
+    });
 }
